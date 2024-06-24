fb7819f4ca660f598eecd3d2e8d71e92b5985cb8
jcl0251 <josephcleal529@gmail.com>
2024-06-23 23:01:59 -0500

    added script to do all required installs

---
+++ install-dependencies.bat
@@ -0,0 +1,12 @@
+@echo off
+
+npm install react-native-safe-area-context
+npm install react-native-screens 
+npm install react-native-safe-area-context
+npm install @react-navigation/material-top-tabs
+npm install react-native-tab-view
+npm install nutrition-label-jquery-plugin
+npm install react-native-webview
+npm install react-native-paper
+npm install expo-font
+npm install expo-splash-screen
